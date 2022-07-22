1403ec9c83f78e87f06d9d2cf120f2cb620a9cd2
mallick-portfolioE <tamal1714019@gmail.com>
2022-07-22 23:11:12 +0600

    updated

--- src/components/SpaceBanner/SpaceBanner.jsx
+++ src/components/SpaceBanner/SpaceBanner.jsx
@@ -5,11 +5,11 @@ const SpaceBanner = () => {
   return (
     <div className="">
       <img className="w-full h-[calc(100vh_-_4rem)]" src={outline} alt="" />
-      <div className="flex justify-between items-center px-12 mx-auto">
-        <div className="w-1/3">
+      <div className="flex flex-col lg:flex-row justify-between items-center px-12 mx-auto">
+        <div className="lg:w-1/3 w-full">
           <h1 className="text-5xl text-primary font-medium">Specifications</h1>
         </div>
-        <div className="w-2/3">
+        <div className="lg:w-2/3 w-full">
           <SpaceFaq />
         </div>
       </div>
