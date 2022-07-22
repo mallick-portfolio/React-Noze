218ecc8021aff7f3322ab1a7bc6aed23dcbf627c
mallick-portfolioE <tamal1714019@gmail.com>
2022-07-22 11:38:11 +0600

    added cart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -43,9 +43,53 @@ const Header = () => {
       </div>
       <div className="flex items-center">
         <h5 className="mr-6 text-[#6b5a78] text-base">Order for $249</h5>
-        <button className="bg-[#654a9b] text-white px-6 py-3 font-bold rounded-md">
-          Pre Order
-        </button>
+        <div className="relative h-full">
+          <button className="bg-[#654a9b] text-white px-6 py-3 font-bold rounded-md">
+            Pre Order
+          </button>
+          <div className="absolute -right-12 -top-2 px-4 py-2 bg-white w-80">
+            <div className="flex justify-between items-center text-xl font-semibold">
+              <h3>Cart</h3>
+              <button>X</button>
+            </div>
+            <div className="flex justify-between flex-col h-[calc(100vh_-_2rem)] py-4">
+              <div className="flex justify-between items-start">
+                <img
+                  className="w-16"
+                  src="https://cdn.shopify.com/s/files/1/0572/5369/1554/products/NOZE-Front_180x180.png?v=1628274438"
+                  alt=""
+                />
+                <div>
+                  <h3 className="text-sm">Noze | Smart Air Quality Monitor</h3>
+                  <div className="flex justify-between items-center">
+                    <div className="flex">
+                      <button className="text-xl font-bold px-2 border">
+                        -
+                      </button>
+                      <button className="border pt-1 px-3">{1}</button>
+                      <button className="text-xl font-bold px-2 border">
+                        +
+                      </button>
+                    </div>
+                    <p>price</p>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="flex justify-between text-sm text-secondary">
+                  <h5 className="">SUBTOTAL</h5>
+                  <p>Price</p>
+                </div>
+                <p className="text-[10px] py-3">
+                  Shipping and discount codes are added at checkout.
+                </p>
+                <button className="bg-[#654a9b] w-full text-white px-6 py-3 font-bold rounded-md">
+                  Pre Order
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
